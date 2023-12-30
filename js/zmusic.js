fa7bfd6bdb15a7519e489e384198df7139e04138
genghenggao <genghenggao@outlook.com>
2023-12-30 19:16:40 +0800

    music

---
+++ js/zmusic.js
@@ -0,0 +1,226 @@
+/*
+design by Voicu Apostol.
+design: https://dribbble.com/shots/3533847-Mini-Music-Player
+I can't find any open music api or mp3 api so i have to download all musics as mp3 file.
+You can fork on github: https://github.com/muhammederdem/mini-player
+*/
+
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      audio: null,
+      circleLeft: null,
+      barWidth: null,
+      duration: null,
+      currentTime: null,
+      isTimerPlaying: false,
+      tracks: [
+        {
+          name: "丫头",
+          artist: "王童语",
+          cover: "./img/0.jpg",
+          source: "./media/丫头.mp3",
+          url: "",
+          favorited: true
+        },
+        {
+          name: "That Gril",
+          artist: "Olly Murs",
+          cover: "./img/2.jpg",
+          source: "./media/that gril.mp3",
+          url: "",
+          favorited: true
+        },
+        {
+          name: "凌晨三点",
+          artist: "陈硕子",
+          cover: "./img/3.jpg",
+          source: "./media/凌晨三点.mp3",
+          url: "",
+          favorited: false
+        },
+        {
+          name: "只想对你说",
+          artist: "掌嘴/Lil-7",
+          cover: "./img/6.jpg",
+          source: "./media/只想对你说.mp3",
+          url: "",
+          favorited: false
+        },
+        {
+          name: "亲爱的你",
+          artist: "小海",
+          cover: "./img/14.jpg",
+          source: "./media/亲爱的你.mp3",
+          url: "",
+          favorited: true
+        },
+        {
+          name: "可能否",
+          artist: "木小雅",
+          cover: "./img/5.jpg",
+          source: "./media/可能否.mp3",
+          url: "",
+          favorited: false
+        },
+        {
+          name: "往后余生",
+          artist: "马良",
+          cover: "./img/15.jpg",
+          source: "./media/往后余生.mp3",
+          url: "",
+          favorited: true
+        },
+        {
+          name: "我的天空",
+          artist: "南征北战",
+          cover: "./img/0.jpg",
+          source: "./media/我的天空.mp3",
+          url: "",
+          favorited: false
+        },
+        {
+          name: "All I have",
+          artist: "光光",
+          cover: "./img/2.jpg",
+          source: "./media/All I have.mp3",
+          url: "",
+          favorited: false
+        },
+        {
+          name: "全部都是你",
+          artist: "DP 龙猪",
+          cover: "./img/3.jpg",
+          source: "./media/全部都是你.mp3",
+          url: "",
+          favorited: false
+        }
+      ],
+      currentTrack: null,
+      currentTrackIndex: 0,
+      transitionName: null
+    };
+  },
+  methods: {
+    play() {
+      if (this.audio.paused) {
+        this.audio.play();
+        this.isTimerPlaying = true;
+      } else {
+        this.audio.pause();
+        this.isTimerPlaying = false;
+      }
+    },
+    generateTime() {
+      let width = (100 / this.audio.duration) * this.audio.currentTime;
+      this.barWidth = width + "%";
+      this.circleLeft = width + "%";
+      let durmin = Math.floor(this.audio.duration / 60);
+      let dursec = Math.floor(this.audio.duration - durmin * 60);
+      let curmin = Math.floor(this.audio.currentTime / 60);
+      let cursec = Math.floor(this.audio.currentTime - curmin * 60);
+      if (durmin < 10) {
+        durmin = "0" + durmin;
+      }
+      if (dursec < 10) {
+        dursec = "0" + dursec;
+      }
+      if (curmin < 10) {
+        curmin = "0" + curmin;
+      }
+      if (cursec < 10) {
+        cursec = "0" + cursec;
+      }
+      this.duration = durmin + ":" + dursec;
+      this.currentTime = curmin + ":" + cursec;
+    },
+    updateBar(x) {
+      let progress = this.$refs.progress;
+      let maxduration = this.audio.duration;
+      let position = x - progress.offsetLeft;
+      let percentage = (100 * position) / progress.offsetWidth;
+      if (percentage > 100) {
+        percentage = 100;
+      }
+      if (percentage < 0) {
+        percentage = 0;
+      }
+      this.barWidth = percentage + "%";
+      this.circleLeft = percentage + "%";
+      this.audio.currentTime = (maxduration * percentage) / 100;
+      this.audio.play();
+    },
+    clickProgress(e) {
+      this.isTimerPlaying = true;
+      this.audio.pause();
+      this.updateBar(e.pageX);
+    },
+    prevTrack() {
+      this.transitionName = "scale-in";
+      this.isShowCover = false;
+      if (this.currentTrackIndex > 0) {
+        this.currentTrackIndex--;
+      } else {
+        this.currentTrackIndex = this.tracks.length - 1;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    nextTrack() {
+      this.transitionName = "scale-out";
+      this.isShowCover = false;
+      if (this.currentTrackIndex < this.tracks.length - 1) {
+        this.currentTrackIndex++;
+      } else {
+        this.currentTrackIndex = 0;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    resetPlayer() {
+      this.barWidth = 0;
+      this.circleLeft = 0;
+      this.audio.currentTime = 0;
+      this.audio.src = this.currentTrack.source;
+      setTimeout(() => {
+        if(this.isTimerPlaying) {
+          this.audio.play();
+        } else {
+          this.audio.pause();
+        }
+      }, 300);
+    },
+    favorite() {
+      this.tracks[this.currentTrackIndex].favorited = !this.tracks[
+        this.currentTrackIndex
+      ].favorited;
+    }
+  },
+  created() {
+    let vm = this;
+    this.currentTrack = this.tracks[0];
+    this.audio = new Audio();
+    this.audio.src = this.currentTrack.source;
+    this.audio.ontimeupdate = function() {
+      vm.generateTime();
+    };
+    this.audio.onloadedmetadata = function() {
+      vm.generateTime();
+    };
+    this.audio.onended = function() {
+      vm.nextTrack();
+      this.isTimerPlaying = true;
+    };
+
+    // this is optional (for preload covers)
+    for (let index = 0; index < this.tracks.length; index++) {
+      const element = this.tracks[index];
+      let link = document.createElement('link');
+      link.rel = "prefetch";
+      link.href = element.cover;
+      link.as = "image"
+      document.head.appendChild(link)
+    }
+  }
+});
