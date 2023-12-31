1123ec5396316d233e2e68451fcb3f7dbaa5a1e6
genghenggao <genghenggao@outlook.com>
2023-12-31 09:43:26 +0800

    修改index.html的照片数量

--- js/earthfunctions.js
+++ js/earthfunctions.js
@@ -45,7 +45,7 @@ var s = setInterval(function() {
 //  生成虚拟数据-修改后新生成虚拟数据- 自动查找image 中的图片 遍历添加进去 
 for (var i = 0; i < 199; i++) {
     personArray.push({
-        image: './img/' + Math.round(Math.random() * 12) + '.jpg'
+        image: './img/' + Math.round(Math.random() * 15) + '.jpg'
     });
 }
 
